8fb70fbb7cae8d7a823781bbe56cf298f34f5964
Jacob Miller <JMILLER@ad.hc-sc.gc.ca>
2019-07-11 16:03:54 -0400

    parser prototype complete

--- melon_markup.py
+++ melon_markup.py
@@ -1,3 +1,5 @@
+import json
+
 class FormatTag():
     def __init__(self, name, start, end, output_template):
         self.name = name
@@ -19,61 +21,60 @@ class FormatTag():
         else:
             return unformatted
 
-    def adjust_openings(self, pos, adjustment):
-        for i, o in enumerate(self.openings):
-            if o > pos:
-                self.openings[i] += adjustment
-
 
 def create_tags():
     tags = []
-
-    name = "bold"
-    start = "[b]"
-    end = "[/b]"
-    output_template = "<strong>{value}</strong>"
-
-    bold = FormatTag(name=name, start=start, end=end, output_template=output_template)
-    tags.append(bold)
-
-    name = "italics"
-    start = "[i]"
-    end = "[/i]"
-    output_template = "<em>{value}</em>"
-
-    italics = FormatTag(name=name, start=start, end=end, output_template=output_template)
-    tags.append(italics)
-
-    name = "url"
-    start = "[url]"
-    end = "[/url]"
-    output_template = "<a href={value}>{value}</a>"
-
-    url = FormatTag(name=name, start=start, end=end, output_template=output_template)
-    tags.append(url)
-
+    with open("tags.txt","r") as f:
+        json_tags = json.loads(f.read())
+        for t in json_tags:
+            tags.append(FormatTag(name=t['name'], start=t['start'], end=t['end'], output_template=t['output_template']))
     return tags
 
 
-tags = create_tags()
-
-
 def parse(s):
+    # the FormatTag class keeps track of information for each tag type, while variables within this function
+    # are responsible for keeping track of inter-tag information and adjusting the tag values based on what happens
+    # as the string is parsed
+    
+    tags = create_tags()
+    
     pos = 0
     while True:
         for tag in tags:
+            # mark location of all start tags as they are identified
             if s[pos:].startswith(tag.start):
                 tag.openings.append(pos)
-                pos += len(tag.start)
+                pos += len(tag.start) # skip to the end of the start tag
                 break
+            
+            # replace start and end tag if both have been found
             elif s[pos:].startswith(tag.end):
-                s = tag.close(s, pos)
-                for stag in tags:
-                    stag.adjust_openings(pos, tag.net_change)
-                pos += tag.net_change + len(tag.end)
-                break
+                if tag.openings:
+                    tag_opening_pos = tag.openings[-1]
+                    s = tag.close(s, pos)
+
+                    # find if any tags are between the start and close of the tag that was just closed
+                    inbetween_tags = False
+                    for _tag in tags:
+                        for i, _tag_opening_pos in enumerate(_tag.openings):
+                            if _tag_opening_pos > tag_opening_pos and _tag_opening_pos < pos:
+                                inbetween_tags = True
+                                del _tag.openings[i] # delete in-between tags as their position value is no longer valid
+                    
+                    if inbetween_tags:
+                    # restart the parser at that the start location of the tag that was closed 
+                        pos = tag_opening_pos
+                        break
+                    
+                    # if there were no tags between, adjust the frame and parsing past the ending of this tag pairing
+                    pos += len(tag.end) + tag.net_change
+                    break
+                else:
+                    # if there is an end tag with no opening tag, skip past it.
+                    pos += len (tag.end)
         else:
             pos += 1
 
         if pos > len(s):
             return s
+        #print(s[:pos], pos)
